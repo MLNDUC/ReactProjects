24fe0678443a7de025f0d26fc470ef22f18eef55
Minh Duc <minhduc.112nrt@gmail.com>
2023-09-10 14:17:03 +0900

    add admin

--- reactRouterSocialapp/src/Footer.jsx
+++ reactRouterSocialapp/src/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   const today = new Date();
   return (
     <footer className="Footer">
-      <p>Copyright &copy; {today.getFullYear()} MLNDUC</p>
+      <p>Copyright &copy; {today.getFullYear()} powered by MLNDUC</p>
     </footer>
   )
 }
